1dea8e40fb31a245275bc7490bfcc32322db28a1
psewasti <psewasti@student.42warsaw.pl>
2026-05-25 17:27:21 +0200

    Make it work with multiple models

--- src/robot.py
+++ src/robot.py
@@ -6,12 +6,24 @@ from .models import FunctionDefinitions, InputPrompt
 
 
 class Robot:
+    answer_start_marker = "<|im_start|>assistant"
+
     def __init__(self, function_definitions: FunctionDefinitions, validator: FSM, printer: Printer):
         import llm_sdk
-        self.model = llm_sdk.Small_LLM_Model()
+        self.model = llm_sdk.Small_LLM_Model("Qwen/Qwen3-0.6B")
+        # self.model = llm_sdk.Small_LLM_Model("openai-community/gpt2")
+        # self.model = llm_sdk.Small_LLM_Model("LiquidAI/LFM2.5-1.2B-Instruct")
+        # self.model = llm_sdk.Small_LLM_Model("TinyLlama/TinyLlama-1.1B-Chat-v0.4")
         self.function_definitions = function_definitions
         self.validator = validator
         self.printer = printer
+
+    def get_answer_start_index(self, tokens: list[int]):
+        answer_start_marker_tokens = self.model.encode(self.answer_start_marker)[0].tolist()
+        for i in range(len(tokens) - len(answer_start_marker_tokens) + 1):
+            if tokens[i:i + len(answer_start_marker_tokens)] == answer_start_marker_tokens:
+                return i + len(answer_start_marker_tokens)
+        raise ValueError("Answer start marker not found in tokens")
 
     def make_prompt(self, input_prompt: InputPrompt) -> str:
         system_prompt = ("You are a helpful assistant that will perform function calling.\n"
@@ -24,8 +36,7 @@ class Robot:
 
         return (f"<|im_start|>system\n{system_prompt}\n<|im_end|>\n"
                 f"<|im_start|>user\n{input_prompt}\n<|im_end|>\n"
-                f"<|im_start|>assistant\n"
-                f"<think>\n\n</think>\n"
+                f"{self.answer_start_marker}"
                 '{\n'
                 f'    "prompt": {input_prompt},\n'
                 '    "name": "'
@@ -33,15 +44,15 @@ class Robot:
 
     def ask(self, prompt: str) -> list[int]:
         tokens = self.model.encode(prompt)[0].tolist()
-        end_think_index = tokens.index(self.model.encode("</think>")[0].tolist()[0])
+        answer_start = self.get_answer_start_index(tokens)
 
         while True:
             logits = self.model.get_logits_from_input_ids(tokens)
             while True:
                 max_logit = logits.index(max(logits))
                 tokens.append(max_logit)
-                validation_result = self.validator.validate_text(self.model.decode(tokens[end_think_index+2:]))
-                self.printer.print(self.model.decode(tokens[end_think_index+2:]))
+                validation_result = self.validator.validate_text(self.model.decode(tokens[answer_start:]))
+                self.printer.print(self.model.decode(tokens[answer_start:]))
                 if validation_result == self.validator.ValidationResult.OK:
                     break
                 if validation_result == self.validator.ValidationResult.FINISHED:
@@ -51,12 +62,8 @@ class Robot:
                 tokens.pop()
 
     def extract_answer(self, tokens: list[int]) -> list[int]:
-        """This function returns the part of the answer that sits between end of thinking and <|im_end|>"""
-        think_end = self.model.encode("</think>")[0].tolist()[0]
-
-        think_end_index = tokens.index(think_end)
-
-        return tokens[think_end_index + 2:]
+        answer_start = self.get_answer_start_index(tokens)
+        return tokens[answer_start:]
 
     def get_answer_to_input_prompt(self, input_prompt: InputPrompt):
         prompt = self.make_prompt(input_prompt)
